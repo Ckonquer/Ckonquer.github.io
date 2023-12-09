cb31a9e94efafeccf0ca80159d82b356231183eb
Ckonquer <forwardseniordev@hotmail.com>
2023-12-09 03:31:35 +0300

    added serveeerless adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,28 +3,30 @@ import sitemap from "@astrojs/sitemap";
 import netlify from "@astrojs/netlify/functions";
 import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
-
 import solidJs from "@astrojs/solid-js";
 import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://Ckonquer.vercel.app",
-  base: "/",
   integrations: [
     sitemap(),
     robotsTxt({
       sitemap: [
-        "https://gianmarco.xyz/sitemap-index.xml",
-        "https://gianmarco.xyz/sitemap-0.xml",
+        // "https://gianmarco.xyz/sitemap-index.xml",
+        // "https://gianmarco.xyz/sitemap-0.xml",
       ],
     }),
     solidJs(),
-    UnoCSS({ injectReset: true }),
+    UnoCSS({
+      injectReset: true,
+    }),
   ],
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
   output: "server",
-  adapter: netlify(),
+  adapter: vercel(),
 });
+
