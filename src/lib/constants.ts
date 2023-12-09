d9730bdfd4aa5764e61ae0e81128fcda3e80d840
Ckonquer <forwardseniordev@hotmail.com>
2023-12-09 03:05:59 +0300

    bulk refactor

---
+++ src/lib/constants.ts
@@ -0,0 +1,19 @@
+export const LINKS = {
+  github: "https://github.com/Ckonquer",
+  linkedin: "https://www.linkedin.com/in/ali-ammar01/",
+};
+
+export const INFO = {
+  name: "Ali Ammar",
+  email: "todo@mail.domain",
+  location: "Detroit, MI",
+  timezone: "America/Detroit",
+  description:
+    "I'm a programming polyglot with experience building full-stack applications, mobile apps, data science operating systems, artificial intelligence, devops and more.",
+};
+
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
